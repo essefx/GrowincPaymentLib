c9025a4cfddec41030415a664b5ad4cf82e7ef5d
Sean A <essefx@gmail.com>
2020-11-27 12:56:20 +0700

    Update 2c2p & duitku

--- examples/TCTP/payment.php
+++ examples/TCTP/payment.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 
 // $init = new \Growinc\Payment\Init('360360000000125', '9E3D31AAA24699C482887E0E47A35DF13F422C40EBF860008B8B8C45347E1C69');
-// $init = new \Growinc\Payment\Init('360360000000200', '4AC61F32A209A56B95712E0394E44AE620DD37ACD27C41AB64F4A99B22751420');
-$init = new \Growinc\Payment\Init('JT03', 'uA7yXRa8PjVe');
+$init = new \Growinc\Payment\Init('360360000000200', '4AC61F32A209A56B95712E0394E44AE620DD37ACD27C41AB64F4A99B22751420');
+// $init = new \Growinc\Payment\Init('JT03', 'uA7yXRa8PjVe');
 $init->setCallbackURL('https://ibank.growinc.dev/oanwef4851ashrb/pg/dk/redapi_result');
 $init->setReturnURL('https://ibank.growinc.dev/oanwef4851ashrb/pg/dk/redapi_form');
 
