509873948bd014d81af6e7185320687d0bf0305d
rch888 <idn.richard@gmail.com>
2020-12-10 14:45:41 +0700

    update payment example ipay88

--- examples/Ipay88/ipay88-payment.php
+++ examples/Ipay88/ipay88-payment.php
@@ -2,12 +2,12 @@
 
   error_reporting(E_ALL);
   ini_set('display_errors', 1);
-
-  require_once __DIR__ . '/../vendor/autoload.php';
+ 
+  require_once __DIR__ . '/../../vendor/autoload.php';
 
   $init = new \Growinc\Payment\Init('ID01625', '1gUbnGkdKA');
   $init->setBaseURI('https://sandbox.ipay88.co.id');
-  $init->setPaymentURL('https://sandbox.ipay88.co.id');
+  $init->setPaymentURL('https://sandbox.ipay88.co.id/ePayment/WebService/PaymentAPI/Checkout');
   $init->setResponseUrl('https://your-site/ipay88-response');
   $init->setBackendURL('https://your-site/ipay88-backend');
 
@@ -16,37 +16,46 @@
   $transaction->setCustomerEmail('lorem@gmail.com');
   $transaction->setCustomerPhone('081298983535');
   $transaction->setDescription('Product B00016 Baju Baru');
-  // $transaction->setOrderID('123039600');
+  
+  /*  paymentType:
 
-  // Maybank VA	9
-  // Mandiri ATM	17
-  // BCA VA	25
-  // BNI VA	26
-  // Permata VA	31
-  $transaction->setPaymentMethod(9);
+  --> bank_transfer
 
-  /**
-   * Dummy items
+    Maybank VA  => bank_transfer,maybankva
+    BCA VA      => bank_transfer,bcava
+    Mandiri ATM => bank_transfer,mandiriatm
+    BNI VA      => bank_transfer,bniva
+    Permata VA  => bank_transfer,permatava
+
+  --> internet_banking 
+  
+    BCA KlikPay => internet_banking,bcakp
+    CIMB Clicks => internet_banking,cimbkp
+    Muamalat IB => internet_banking,muamalatkp
+    Danamon Online internet_banking,Banking => danamonkp
+
+  --> credit_card
   */
-  $detail_item = [
-    ["id" => "item01", "price" => 50000, "quantity" => 2, "name" => "Ayam Zozozo"],
-    ["id" => "item01", "price" => 30000, "quantity" => 5, "name" => "Ayam Zozozo"]
+
+  $transaction->setPaymentMethod('bank_transfer,permata');
+
+  // $transaction->setPaymentId(9);
+  // $transaction->setPaymentId(9);
+
+  $item_detail = [
+    ["id" => "a1", "price" => 50000, "quantity" => 5, "name" => "apel", "brand" => "Fuji Apple", "category" => "Fruit","merchant_name" => "Fruit-store"] //only cc
   ];
-  // set items
-  $transaction->setItem($detail_item);
+  $transaction->setItem($item_detail);
+  
   $vendor = new \Growinc\Payment\Vendors\Ipay88($init);
 
-  // $result = $vendor->SecurePayment($transaction);
-  // print_r($result);exit();
   try {
-
+    
     $result = $vendor->SecurePayment($transaction); // return payment URL
-    // $result = $vendor->RedirectPayment($transaction); // redirect to vendor URL
     extract($result);
     print_r($response);
-    //
-    // print_r($vendor->getRequest());
-    // print_r($vendor->getResponse()); // Get  PSR7 object
+
   } catch (\Throwable $e) {
     echo 'Payment failed: ' . $e->getCode();
   }
+  
