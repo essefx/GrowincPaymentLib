aa57d19b89989106de34249bccddbc5e47854a62
Sean A <essefx@gmail.com>
2020-12-16 11:42:51 +0700

    Update to Transaction, Midtrans, Xendit

--- examples/Xendit/xendit-payment.php
+++ examples/Xendit/xendit-payment.php
@@ -1,33 +1,50 @@
-<?php 
+<?php
 
-// require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-$init = new \Growinc\Payment\Init('xnd_development_8OoTlgwIthHrTr7R9gg0AIUhH2PAAPjAdReAltPc7yQxzBlRnhAmYwdGqn6vG4Y','xnd_public_development_UmKcL9LSSd96GKqb7ZN3UNZIPNUClJIBd4ndBoqFPRpqslgJ5q7GzCV0lWMOXRZy'); /*(secret_key , public_key)*/
+$init = new \Growinc\Payment\Init('xnd_development_8OoTlgwIthHrTr7R9gg0AIUhH2PAAPjAdReAltPc7yQxzBlRnhAmYwdGqn6vG4Y', 'xnd_public_development_UmKcL9LSSd96GKqb7ZN3UNZIPNUClJIBd4ndBoqFPRpqslgJ5q7GzCV0lWMOXRZy'); /*(secret_key , public_key)*/
 $init->setBaseURI('https://api.xendit.co');
 $init->setPaymentURL('https://api.xendit.co');
 
 $transaction = new \Growinc\Payment\Transaction();
-$transaction->setCustomerName('LOREM');
+$transaction->setCustomerName('LOREM IPSUM');
 $transaction->setCustomerEmail('lorem@ipsum.com');
-$transaction->setCustomerPhone('081298983535');
+$transaction->setCustomerPhone('088812345678');
 $transaction->setAmount(100000);
 $transaction->setCountrycode('IDN');
-// $transaction->setVaName('Lorem Ipsum');
 
-/*	VA
-	BCA , MANDIRI , BNI , BRI , PERMATA
-*/
-// xendit param required
-// $transaction->setPaymentMethod('bca'); // for VA 
-// $transaction->setPaymentType('bank_transfer');
+// Payment Method Supported:
+// 1. bank_transfer (VA) ----------- CURRENTLY ONLY THIS SUPPORTED
+// 	bca
+// 	bni
+// 	bri
+// 	mandiri
+// 	permata
+// 	sahabat_sampoerna
+// 2. credit_card
+// 3. ewallet
+// 	ovo
+// 	linkaja
+// 	shopeepay
+// 3. qr (QRIS)
+// 4. cstore (retail)
+// 	alfamart
+// 	indomaret
+
 $transaction->setPaymentMethod('bank_transfer,bca');
+// $transaction->setPaymentMethod('bank_transfer,mandiri');
+// $transaction->setPaymentMethod('bank_transfer,permata');
+// $transaction->setPaymentMethod('credit_card');
+// $transaction->setPaymentMethod('ewallet,ovo');
+// $transaction->setPaymentMethod('ewallet,linkaja');
+// $transaction->setPaymentMethod('qris');
+// $transaction->setPaymentMethod('cstore,indomaret');
+// $transaction->setPaymentMethod('cstore,alfamart');
+
 $transaction->setDescription('Pembelian Elektronik');
 
 $vendor = new \Growinc\Payment\Vendors\Xendit($init);
-// $result = $vendor->SecurePayment($transaction);
-// print_r($result);exit();
- 
+
 try {
 	$result = $vendor->SecurePayment($transaction); // return payment URL
 	// extract($result);
@@ -39,6 +56,3 @@ try {
 } catch (\Throwable $e) {
 	echo 'Payment failed: ' . $e->getCode();
 }
-  
-
-?>    
